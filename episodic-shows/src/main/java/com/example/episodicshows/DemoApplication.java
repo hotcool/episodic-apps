ffaa3cf69c6777a1f8154954faded8560b3d427f
trainer4 <trainer4@trainer4s-MacBook-Pro.local>
2017-05-24 15:06:31 -0600

    Add Eureka

--- episodic-shows/src/main/java/com/example/episodicshows/DemoApplication.java
+++ episodic-shows/src/main/java/com/example/episodicshows/DemoApplication.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
